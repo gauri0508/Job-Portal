a18ce76c8c3b01dbac2d8ddb763da0fdc861c734
Gauri Shukla <gaurishukla0508@gmail.com>
2025-04-21 22:00:29 +0530

    Fix for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/Job-Portal/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
